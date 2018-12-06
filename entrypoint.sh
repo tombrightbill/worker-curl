15cb5478506c1433abbdb8fc0890b49d29b3f652
Tom Brightbill <tbrightbill@cloudflare.com>
2018-12-06 15:28:21 -0800

    common denom

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
-	echo "$GITHUB_REF was not master, exiting..."
-	exit 0
-fi
+set -e
 
-echo "On branch ${GITHUB_REF}, deploying..."
-
-curl -H "x-auth-key: $(API_KEY)" -H "x-auth-email: $(EMAIL)" -vso /dev/null "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/workers/script" --data-binary "@$(WORKER_SCRIPT)"
+sleep "$*"
